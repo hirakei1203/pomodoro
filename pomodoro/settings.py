82fc468f0c9473e4d4f03b60c43c4e9de206ef25
hirakei1203 <56023847+hirakei1203@users.noreply.github.com>
2020-01-07 22:31:56 +0900

    materializecss

--- pomodoro/settings.py
+++ pomodoro/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'timer.apps.TimerConfig'
+    'timer.apps.TimerConfig',
+    'materializecssform',
 ]
 
 MIDDLEWARE = [
