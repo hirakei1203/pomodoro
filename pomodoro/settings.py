a3db8337d142d05f5cc295445b255a4f98636296
hirakei1203 <56023847+hirakei1203@users.noreply.github.com>
2020-01-07 23:11:48 +0900

    mate

--- pomodoro/settings.py
+++ pomodoro/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # 'materializecssform',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'timer.apps.TimerConfig',
-    'materializecssform',
 ]
 
 MIDDLEWARE = [
