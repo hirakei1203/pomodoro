1bda748033b4db1c7f01b8e4c652d3fdd4537b4f
hirakei1203 <56023847+hirakei1203@users.noreply.github.com>
2020-01-09 20:36:51 +0900

    login1

--- timer/views.py
+++ timer/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 def index(request):
     return render(request, "timer/index.html")
+
+def signup(request):
+  if request.mtehod == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user_instacnce =  form.save()
+      login()
 
 # Create your views here.
