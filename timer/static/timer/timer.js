77effe8e9c018c142cafe058a4d360d6feb6439d
hirakei1203 <56023847+hirakei1203@users.noreply.github.com>
2020-01-11 14:33:22 +0900

    js2

--- timer/static/timer/timer.js
+++ timer/static/timer/timer.js
@@ -1,11 +1,14 @@
 $(function(){
 
+  // ドロップダウンを作る場合、以下コードとmaterializeを活用する
   $(".dropdown-trigger").dropdown();
 
+  console.log("aaa");
 
   $(".task_min").on("keyup", function(){
-    let task_setting_min = S(this).val();
+    let task_setting_min = $(this).val();
     console.log(task_setting_min);
+    $(".timeleft_min").html(task_setting_min);
   })
 
 })
