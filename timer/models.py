47455fc70abaf1dbdb8ee545cd445295507d5604
hirakei1203 <56023847+hirakei1203@users.noreply.github.com>
2020-01-15 00:19:12 +0900

    model6

--- timer/models.py
+++ timer/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import AbstractUser, UserManager
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from model_utils.fields import StatusField
+from model_utils import Choices
 
 
 class UserManager(UserManager):
