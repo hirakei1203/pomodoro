a8c4332b38b6d7ec82482261460384a11cd1fd63
hirakei1203 <56023847+hirakei1203@users.noreply.github.com>
2020-01-18 14:16:44 +0900

    admin add

--- timer/admin.py
+++ timer/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import User
+from .models import User, Setlist, Record
 
 admin.site.register(User)
-
+admin.site.register(Setlist)
+admin.site.register(Record)
 # Register your models here.
