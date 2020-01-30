d27042325dd8a05e1e2c49c394bb6dbe2698b111
hirakei1203 <56023847+hirakei1203@users.noreply.github.com>
2020-01-30 22:24:55 +0900

    login path

--- timer/urls.py
+++ timer/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('setlist/<int:pk>/', views.SetlistDetailView.as_view(), name="user_timer_page"),
     path('signup/', views.signup, name='signup'),
     path('ajax/', views.test_ajax_response),
+    path('login/', views.Login.as_view,(), name='login'),
+    path('logout/', views.Logout.as_view(), name='logout'),
     ]
